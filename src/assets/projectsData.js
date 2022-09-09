fe77b66171b4d3c171f143c7b4c5194deea434b4
lucaseduardolema <lucaseduardolema@gmail.com>
2022-09-08 22:26:06 -0300

    add recipes app project

--- src/assets/projectsData.js
+++ src/assets/projectsData.js
@@ -2,25 +2,20 @@ import spotify from "./clone-spotify.png";
 import comercio from "./comercio.png";
 import trybewallet from "./trybewallet.png";
 import  trybetunes from './trybetunes.png'
-import inprogress from "./inprogress.png";
 import starwars from "./starwars.png"
+import recipes from './recipesApp.JPG'
 
 const projectsData = [
   {
-    name: "TrybeTunes",
-    ghDeploy: "https://lucaseduardolema.github.io/trybetunes/",
-    img: trybetunes,
-    text: "Neste projeto criei uma aplicação React Class Components, utilizando o estado local dos componentes para reproduzir músicas das mais variadas bandas e artistas, criar uma lista de músicas favoritas e editar o perfil da pessoa usuária logada. Confira o Readme.md para mais informações.",
-    ghCode: "https://github.com/lucaseduardolema/trybetunes",
+    id: 0,
+    name: "App de Recitas",
+    ghDeploy: "https://lucaseduardolema.github.io/recipes-app/",
+    img: recipes,
+    text: "Nesse projeto desenvolvi um app de receitas em React com foco em dispositivos mobile, originalmente um projeto em grupo, desenvolvendo suas funcionalidades de acordo com demandas definidas em um quadro Kanban e metodologias ágeis, em um cenário próximo ao do mercado de trabalho. Agora refatorado por mim para que eu treinasse o uso Hooks em conjunto com o Redux, nele é possível: ver, buscar, filtrar, favoritar e acompanhar o progresso de preparação de receitas e drinks, sua base de dados são 2 APIs distintas, uma para comidas e outra para bebidas.",
+    ghCode: "https://github.com/lucaseduardolema/recipes-app",
   },
   {
-    name: "TrybeWallet",
-    ghDeploy: "https://lucaseduardolema.github.io/trybewallet/",
-    img: trybewallet,
-    text: "Nesse projeto desenvolvi uma aplicação React Class Components em conjunto com o Redux, trata-se de uma carteira de controle de gastos com conversor de moedas em que o usuário é capaz de adicionar, remover e editar um gasto, visualizar uma tabela com seus gastos, visualizar o total de gastos convertidos para o real, tais dados são consumidos via API e todas as ações são gerenciadas pelo Redux por meio de um store global, reducers, dispatchers e actions síncronas e assíncronas. Confira o Readme.md para mais informações.",
-    ghCode: "https://github.com/lucaseduardolema/trybewallet",
-  },
-  {
+    id: 1,
     name: "Star Wars Planets",
     ghDeploy: "https://lucaseduardolema.github.io/star-wars-planets/",
     img: starwars,
@@ -28,25 +23,36 @@ const projectsData = [
     ghCode: "https://github.com/lucaseduardolema/star-wars-planets",
   },
   {
-    name: "App de Recitas",
-    ghDeploy: "",
-    img: inprogress,
-    text: "Nesse projeto desenvolvi um app de receitas, utilizando o que há de mais moderno dentro do ecossistema React: Hooks e Context API! Nele é possível: ver, buscar, filtrar, favoritar e acompanhar o progresso de preparação de receitas e drinks! A base de dados serão 2 APIs distintas, uma para comidas e outra para bebidas e o layout tem como foco dispositivos móveis.",
-    ghCode: "",
+    id: 2,
+    name: "TrybeTunes",
+    ghDeploy: "https://lucaseduardolema.github.io/trybetunes/",
+    img: trybetunes,
+    text: "Neste projeto criei uma aplicação React Class Components, utilizando o estado local dos componentes para reproduzir músicas das mais variadas bandas e artistas, criar uma lista de músicas favoritas e editar o perfil da pessoa usuária logada. Confira o Readme.md para mais informações.",
+    ghCode: "https://github.com/lucaseduardolema/trybetunes",
   },
   {
+    id: 3,
+    name: "TrybeWallet",
+    ghDeploy: "https://lucaseduardolema.github.io/trybewallet/",
+    img: trybewallet,
+    text: "Nesse projeto desenvolvi uma aplicação React Class Components em conjunto com o Redux, trata-se de uma carteira de controle de gastos com conversor de moedas em que o usuário é capaz de adicionar, remover e editar um gasto, visualizar uma tabela com seus gastos, visualizar o total de gastos convertidos para o real, tais dados são consumidos via API e todas as ações são gerenciadas pelo Redux por meio de um store global, reducers, dispatchers e actions síncronas e assíncronas. Confira o Readme.md para mais informações.",
+    ghCode: "https://github.com/lucaseduardolema/trybewallet",
+  },
+  {
+    id: 4,
     name: "Spotify Clone",
     ghDeploy: "https://lucaseduardolema.github.io/spotify-clone/",
     img: spotify,
-    text: "Clone o serviço de streaming de música Spotify",
+    text: "Clone o serviço de streaming de música Spotify, projeto simples feito em HTML, CSS e JavaScript puros.",
     ghCode:
       "https://github.com/lucaseduardolema/lucaseduardolema.github.io/tree/main/spotify-clone",
   },
   {
+    id: 5,
     name: "Página de Comércio",
     ghDeploy: "https://lucaseduardolema.github.io/saru/",
     img: comercio,
-    text: "Página de um estabelecimento comercial",
+    text: "Página de um estabelecimento comercial, projeto simples feito em HTML, CSS e JavaScript puros.",
     ghCode:
       "https://github.com/lucaseduardolema/lucaseduardolema.github.io/tree/main/saru",
   },
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,9 +1,19 @@
-import React from "react";
-import { Card, Col, Container, Row } from "react-bootstrap";
+import React, { useState } from "react";
+import { Card, Col, Container, Modal, Row } from "react-bootstrap";
 import projectsData from "../assets/projectsData";
 import "../styles/Projects.css";
 
 function Projects() {
+  const [showModal, setShowModal] = useState(
+    projectsData.reduce((acc, curr) => {
+      acc = {
+        ...acc,
+        [curr.id]: false,
+      }
+      return acc
+    }, {})
+  );
+
   return (
     <Container id="projects">
       <Row>
@@ -12,17 +22,32 @@ function Projects() {
         </Col>
       </Row>
       <Row xs={1} md={2} className="g-4">
-        {projectsData.map(({ name, ghDeploy, img, ghCode, text }) => (
+        {projectsData.map(({ id, name, ghDeploy, img, ghCode, text }) => (
           <Col key={name}>
             <Card>
-              <Card.Img variant="top" src={img} />
-              <Card.Title>{name}</Card.Title>
-              <Card.Text>{text}</Card.Text>
+              <Card.Img onClick={() => setShowModal( { ...showModal, [id]: true  } )} variant="top" src={img} />
+
+              <Modal
+                size="lg"
+                show={ showModal[id] }
+                onHide={() => setShowModal( { ...showModal, [id]: false } )}
+                aria-labelledby={`${name}-modal`}
+              >
+                <Modal.Header closeButton>
+                  <Modal.Title id={`${name}-modal`}>
+                    {name}
+                  </Modal.Title>
+                </Modal.Header>
+                <img className="img-fluid" src={ img } alt={ name } ></img>
+              </Modal>
+
+              <Card.Title className="m-3">{name}</Card.Title>
+              <Card.Text className="m-3">{text}</Card.Text>
               <a
                 href={ghDeploy}
                 target="_blank"
                 rel="noreferrer"
-                className="btn btn-outline-success"
+                className="btn btn-outline-success mb-2"
               >
                 Ver Live Demo
               </a>
